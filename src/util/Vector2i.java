3280ab51d109911af37f852eec0ed6018b0f2207
dalton <daltonjriley@gmail.com>
2025-12-31 13:03:07 -0600

    Vector Class

---
+++ src/util/Vector2i.java
@@ -0,0 +1,53 @@
+package util;
+
+public class Vector2i {
+
+    private int x;
+    private int y;
+    
+    // Constructors
+    public Vector2i() {
+        this.x = 0;
+        this.y = 0; 
+    }
+    public Vector2i(int x, int y) {
+        this.x = x;
+        this.y = y; 
+    }
+    public Vector2i(Vector2i other) {
+        this.x = other.x;
+        this.y = other.y; 
+    }
+
+    // Getters and Setters
+    public void set(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+    public Vector2i setX(int x) {
+        this.x = x;
+        return this;
+    }
+    public int getY() {
+        return y;
+    }
+    public Vector2i setY(int y) {
+        this.y = y;
+        return this;
+    }
+
+    // Vector Operations
+    public Vector2i add(Vector2i other) {
+        this.x += other.x;
+        this.y += other.y;
+        return this;
+    }
+    public Vector2i subtract(Vector2i other) {
+        this.x -= other.x;
+        this.y -= other.y;
+        return this;
+    }
+}
